1b424b8f235b944982a5ba65d99bfd9aab016bdf
Will Haldean Brown <will.h.brown@gmail.com>
2014-10-05 00:41:27 -0700

    Draggin and droppin.

--- web/static/chess.js
+++ web/static/chess.js
@@ -4,20 +4,46 @@ function loc_str(rank, file) {
     return file_str + rank_str
 }
 
+function file_from_str(str) {
+    return str.charCodeAt(0) - "a".charCodeAt(0);
+}
+
+function rank_from_str(str) {
+    return str[1] - 1;
+}
+
 $(document).ready(function() {
     var board_elem = document.getElementById("board");
+    var current_board = undefined;
     function load_board(b) {
         for (var r = 0; r < 8; r++) {
             for (var f = 0; f < 8; f++) {
                 var square_elem = document.getElementById(loc_str(r, f));
                 var piece = b[r*8+f];
                 if (piece) {
-                    square_elem.innerHTML = "<img src='/static/" + piece + ".svg'>";
+                    square_elem.innerHTML = (
+                        "<img class='piece' src='/static/" + piece + ".svg' " +
+                        "data-piece='" + piece + "' " +
+                        "data-rank='" + r + "' data-file='" + f + "'>");
                 } else {
                     square_elem.innerHTML = "";
                 }
             }
         }
+        $(".piece").draggable();
+        $("#board td").droppable({
+            drop: function(ev, ui) {
+                var elem = ui.draggable;
+                var r = elem.data().rank;
+                var f = elem.data().file;
+                var new_r = rank_from_str(ev.target.id);
+                var new_f = file_from_str(ev.target.id);
+                current_board[new_r * 8 + new_f] = current_board[r * 8 + f];
+                current_board[r * 8 + f] = "";
+                load_board(current_board);
+            }
+        });
+        current_board = b;
     }
 
     test_board = [
