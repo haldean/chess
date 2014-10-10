1c1d1422bbc3777b783401a06e6458b3cd5f5433
Will Haldean Brown <will.h.brown@gmail.com>
2014-10-09 21:56:33 -0700

    More web changes.

--- web/web.py
+++ web/web.py
@@ -2,10 +2,24 @@ import flask
 import os
 
 app = flask.Flask("chess")
+sqlc = psychopg2.connect("dbname='chess' host='localhost'")
 
 @app.route("/")
 def index():
     return flask.render_template("index.html")
+
+@app.route("/game")
+def game():
+    return flask.render_template("game.html")
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+@app.route("/do_login", methods=["POST"])
+def do_login():
+    user = flask.request.form["user"]
+    password = flask.request.form["pass"]
 
 if __name__ == "__main__":
     # Jinja does some funny shit here; just set the app directory to the
