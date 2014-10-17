93456ddbbff0c7a5d86e82f2185c9c5dfb27a1c6
Will Haldean Brown <will.h.brown@gmail.com>
2014-10-17 11:26:38 -0700

    Change deploy scripts to delete existing containers if necessary.

--- deploy/start-frontend.sh
+++ deploy/start-frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo docker stop chess-frontend && sudo docker rm chess-frontend
 sudo docker run -d -p 5000:5000 --link chess-redis:chess-redis \
     --name chess-frontend haldean/chess-frontend \
     python /chess/start.py --redis_host chess-redis
--- deploy/start-redis.sh
+++ deploy/start-redis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo docker stop chess-redis && sudo docker rm chess-redis
 sudo docker run \
     --name chess-redis -v `pwd`/local-data/redis/:/data -d \
     redis:2.8 redis-server --appendonly yes
