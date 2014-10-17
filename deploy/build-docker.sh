8d9ce494eb81efeaf583df66990626fc895eef7e
Will Haldean Brown <will.h.brown@gmail.com>
2014-10-17 11:20:57 -0700

    Add manual game entry.

--- deploy/build-docker.sh
+++ deploy/build-docker.sh
@@ -7,7 +7,6 @@ RUN apt-get update && apt-get install -y python python-pip git python-dev
 RUN git clone https://github.com/haldean/chess /chess
 RUN pip install -r /chess/requirements.txt
 RUN mkdir /var/log/chess
-RUN python /chess/load_eco.py
 EOF
 # This is a bit of a hack; we do this after the initial clone and pip install to
 # let us use the cache from the installation earlier; for new images, the git
@@ -16,6 +15,7 @@ EOF
 echo "RUN echo Building for git commit $(git rev-parse HEAD)" >> Dockerfile
 cat << EOF >> Dockerfile
 RUN cd /chess; git pull origin master
+RUN python /chess/load_eco.py
 ADD api_keys.py /chess/api_keys.py
 EOF
 
