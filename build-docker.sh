fee6aa232b37255de33270489b632050c3e483ee
Will Haldean Brown <will.h.brown@gmail.com>
2014-10-17 08:18:36 -0700

    Even better docker caching; docker images work now.

--- build-docker.sh
+++ build-docker.sh
@@ -6,6 +6,8 @@ MAINTAINER Haldean Brown <me@haldean.org>
 RUN apt-get update && apt-get install -y python python-pip git python-dev
 RUN git clone https://github.com/haldean/chess /chess
 RUN pip install -r /chess/requirements.txt
+RUN mkdir /var/log/chess
+RUN python /chess/load_eco.py
 EOF
 # This is a bit of a hack; we do this after the initial clone and pip install to
 # let us use the cache from the installation earlier; for new images, the git
@@ -14,8 +16,6 @@ EOF
 echo "RUN echo Building for git commit $(git rev-parse HEAD)" >> Dockerfile
 cat << EOF >> Dockerfile
 RUN cd /chess; git pull origin master
-RUN mkdir /var/log/chess
-RUN python /chess/load_eco.py
 ADD api_keys.py /chess/api_keys.py
 EOF
 
